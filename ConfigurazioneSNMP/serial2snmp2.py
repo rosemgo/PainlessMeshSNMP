da4aa40c650cd6e4b331ab0ca5339c68fe849905
rosemgo <32515348+rosemgo@users.noreply.github.com>
2021-06-03 20:39:11 +0200

    ConfigurazioneSNMP

---
+++ ConfigurazioneSNMP/serial2snmp2.py
@@ -0,0 +1,44 @@
+#LO SCRIPT E' USATO PER LEGGERE DA PORTA SERIALE, QUINDI VA ESEGUITO DOPO AVER COLLEGATO IL DISPOSITIVO SNIFFER AL PC TRAMITE PORTA USB
+
+import serial
+import time
+
+TRAPSRV = "192.168.204.128" # impostazione dell'indirizzo del dipositivo esterno
+LOCALIP = "192.168.1.12"
+
+## PRENDE INPUT DA SERIALE
+# apertura porta seriale
+s = serial.Serial('com7', 115200)
+
+print("serial:", s)
+time.sleep(0.2)
+print("inizio lettura")
+
+# esecuzione continua
+while True:
+    #dati = s.readline().decode('ascii')
+    #print("dati: ", dati)
+
+    #ricezione dati dalla porta seriale
+    str=s.readline().decode()
+    print(str)
+    
+    ## INVIO NOTIFICA SNMP TRAP AL SERVER ##
+    from pysnmp.hlapi import * #importa tutto dalla libreria pysnmp.hlapi
+
+    ## invio dati al destinatario, con un tentativo di mascheramento ##
+    iterator = sendNotification(
+        SnmpEngine(),
+        CommunityData('public', mpModel=0),
+        UdpTransportTarget((TRAPSRV, 162)),
+        ContextData(),
+        'trap',
+        NotificationType(
+            ObjectIdentity('1.3.6.1.2.1.1')
+        ).addVarBinds(
+            ('1.3.6.1.6.3.18.1.3.0', '192.168.122.1'),
+            ('1.3.6.1.2.1.1.1', str),        #sysdescr
+        )
+    )
+
+    errorIndication, errorStatus, errorIndex, varBinds = next(iterator) 
